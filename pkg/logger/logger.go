9d499de4c2966eaca2d1065252c8b975bcf43171
Yaroslav Muravsky <y@myarik.com>
2018-03-05 19:16:01 +0200

    Update the logger pkg

--- pkg/logger/logger.go
+++ pkg/logger/logger.go
@@ -1,1 +1,35 @@
 package logger
+
+import (
+	"io"
+	"log"
+	"bytes"
+)
+
+type Log struct {
+	logLevel int
+	logger   *log.Logger
+}
+
+func New(out io.Writer, level int) *Log {
+	return &Log{
+		logLevel: level,
+		logger:   log.New(out, "", log.LstdFlags),
+	}
+}
+
+func (l Log) log(logLevel, message string) {
+	var buffer bytes.Buffer
+	buffer.WriteString(logLevel)
+	buffer.WriteString(": ")
+	buffer.WriteString(message)
+	l.logger.Println(buffer.String())
+}
+
+func (l Log) Debug(message string) {
+	l.log("DEBUG", message)
+}
+
+func (l Log) Info(message string) {
+	l.log("INFO", message)
+}
