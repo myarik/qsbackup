b2f8cad924ead8c2c093a043e1cebeee56e75af0
Yaroslav Muravsky <y@myarik.com>
2018-03-17 11:32:38 +0200

    Add the BackupConfig structure

--- cmd/qsbackup/main.go
+++ cmd/qsbackup/main.go
@@ -3,9 +3,15 @@ package main
 import (
 	log "github.com/myarik/qsbackup/pkg/logger"
 	"os"
+	"github.com/myarik/qsbackup/cmd/qsbackup/config"
 )
 
 func main()  {
+
+	tc := &config.BackupConfig{
+		Name: "Test vasya",
+	}
+
 	logger := log.New(os.Stdout, 0)
-	logger.Debug("Test")
+	logger.Debug(tc.Logfile)
 }
